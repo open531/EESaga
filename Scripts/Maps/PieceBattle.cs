51544766ae70d81b336b93716eb52337eb45db96
Xuc Pan <panxuc0618@gmail.com>
2024-02-08 16:54:33 +0800

    feat: :sparkles: improve battle manager
    UNFINISHED

--- Scripts/Maps/PieceBattle.cs
+++ Scripts/Maps/PieceBattle.cs
@@ -4,6 +4,7 @@ using Entities;
 using Entities.BattleEnemies;
 using Entities.BattleParties;
 using Godot;
+using Interfaces;
 using System.Collections.Generic;
 using Utilities;
 
@@ -13,38 +14,49 @@ public partial class PieceBattle : Node2D
     public List<IBattlePiece> Pieces { get; set; }
     public List<IBattleParty> Parties { get; set; }
     public List<IBattleEnemy> Enemies { get; set; }
+    public List<Obstacle> Obstacles { get; set; }
+    public List<Trap> Traps { get; set; }
+
+    private Room _room;
 
     private static readonly PackedScene _battlePartyScene = GD.Load<PackedScene>("res://Scenes/Entities/BattleParties/battle_party.tscn");
     private static readonly PackedScene _playerBattleScene = GD.Load<PackedScene>("res://Scenes/Entities/BattleParties/player_battle.tscn");
     private static readonly PackedScene _battleEnemyScene = GD.Load<PackedScene>("res://Scenes/Entities/BattleEnemies/battle_enemy.tscn");
     public override void _Ready()
     {
-        foreach (var party in GameData.Parties)
-        {
-            var battleParty = party.PartyName switch
-            {
-                var partyName when partyName == GameData.Player.PlayerName => _playerBattleScene.Instantiate<PlayerBattle>(),
-                _ => _battlePartyScene.Instantiate<BattleParty>()
-            };
-            battleParty.Initialize(party);
-            battleParty.Name = "BattleParty" + battleParty.PartyName;
-            //TileMap.Parties.AddChild(battleParty);
-            Parties.Add(battleParty);
-        }
+    }
 
-        // TODO: Load Enemy from Room?
+    public void Initialize(Room room)
+    {
+        _room = room;
+        room.RemoveChild(room.TileMap);
+        AddChild(room.TileMap);
+        //foreach (var party in GameData.Parties)
+        //{
+        //    var battleParty = party.PartyName switch
+        //    {
+        //        var partyName when partyName == GameData.Player.PlayerName => _playerBattleScene.Instantiate<PlayerBattle>(),
+        //        _ => _battlePartyScene.Instantiate<BattleParty>()
+        //    };
+        //    battleParty.Initialize(party);
+        //    battleParty.Name = "BattleParty" + battleParty.PartyName;
+        //    //TileMap.Parties.AddChild(battleParty);
+        //    Parties.Add(battleParty);
+        //}
 
-        foreach (var party in Parties)
-        {
-            Pieces.Add(party);
-        }
-        foreach (var enemy in Enemies)
-        {
-            Pieces.Add(enemy);
-        }
+        //// TODO: Load Enemy from Room?
 
-        Parties.Sort((x, y) => x.Agility - y.Agility);
-        Enemies.Sort((x, y) => x.Agility - y.Agility);
-        Pieces.Sort((x, y) => x.Agility - y.Agility);
+        //foreach (var party in Parties)
+        //{
+        //    Pieces.Add(party);
+        //}
+        //foreach (var enemy in Enemies)
+        //{
+        //    Pieces.Add(enemy);
+        //}
+
+        //Parties.Sort((x, y) => x.Agility - y.Agility);
+        //Enemies.Sort((x, y) => x.Agility - y.Agility);
+        //Pieces.Sort((x, y) => x.Agility - y.Agility);
     }
 }
