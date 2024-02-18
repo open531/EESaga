0c3fc02647813ec05d114621f9ae0aa43811522e
Xuc Pan <panxuc0618@gmail.com>
2024-02-18 20:12:52 +0800

    feat: :sparkles: add layer

---
+++ Scripts/Layer.cs
@@ -0,0 +1,45 @@
+namespace EESaga.Scripts;
+
+using Godot;
+
+public partial class Layer : CanvasLayer
+{
+    private ColorRect _blackRect;
+
+    [Signal] public delegate void FadedInEventHandler();
+    [Signal] public delegate void FadedOutEventHandler();
+
+    public static Layer Instance() => GD.Load<PackedScene>("res://Scenes/layer.tscn").Instantiate<Layer>();
+
+    public override void _Ready()
+    {
+        _blackRect = GetNode<ColorRect>("BlackRect");
+        _blackRect.Color = new Color(0, 0, 0, 0);
+    }
+
+    public void FadeIn(float duration = 0.5f)
+    {
+        var tween = CreateTween();
+        tween.TweenProperty(_blackRect, "color", new Color(0, 0, 0, 1), duration);
+        tween.Finished += () => EmitSignal(SignalName.FadedIn);
+    }
+
+    public void FadeOut(float duration = 0.5f)
+    {
+        var tween = CreateTween();
+        tween.TweenProperty(_blackRect, "color", new Color(0, 0, 0, 0), duration);
+        tween.Finished += () => EmitSignal(SignalName.FadedOut);
+    }
+
+    public async void FadeInAndOut(float duration = 0.5f)
+    {
+        var fadeInTween = CreateTween();
+        fadeInTween.TweenProperty(_blackRect, "color", new Color(0, 0, 0, 1), duration);
+        await ToSignal(fadeInTween, "finished");
+        EmitSignal(SignalName.FadedIn);
+        var fadeOutTween = CreateTween();
+        fadeOutTween.TweenProperty(_blackRect, "color", new Color(0, 0, 0, 0), duration);
+        await ToSignal(fadeOutTween, "finished");
+        EmitSignal(SignalName.FadedOut);
+    }
+}
