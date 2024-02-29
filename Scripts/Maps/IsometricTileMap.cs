5cd846e14dfe07182372ef4838efb6cb68fe7282
Xuc Pan <panxuc0618@gmail.com>
2024-02-29 16:40:39 +0800

    Revert "feat: :sparkles: update SelectedCell"
    This reverts commit d790c86d5ae4be6eb9d941aa9fa4367d376f9343.

--- Scripts/Maps/IsometricTileMap.cs
+++ Scripts/Maps/IsometricTileMap.cs
@@ -1,10 +1,13 @@
 namespace EESaga.Scripts.Maps;
 
+using Entities;
 using Godot;
 using System.Collections.Generic;
 
 public partial class IsometricTileMap : TileMap
 {
+    public Vector2I? SelectedCell { get; private set; } = null;
+
     /// <summary>
     /// 可以用来放置棋子的地图坐标
     /// </summary>
@@ -34,6 +37,11 @@ public partial class IsometricTileMap : TileMap
         UpdateCells();
         UpdateAStar();
         GenerateBoundary();
+    }
+
+    public override void _Process(double delta)
+    {
+        UpdateSelectedTile();
     }
 
     private void GenerateBoundary()
@@ -110,6 +118,29 @@ public partial class IsometricTileMap : TileMap
                 !AvailableCells.Contains(cell))
             {
                 AvailableCells.Add(cell);
+            }
+        }
+    }
+
+    private void UpdateSelectedTile()
+    {
+        var mousePos = GetGlobalMousePosition();
+        var tileMapPos = LocalToMap(mousePos);
+        if (tileMapPos != SelectedCell)
+        {
+            if (SelectedCell != null)
+            {
+                SetCell((int)Layer.Cursor, (Vector2I)SelectedCell, TileSelectedId, null);
+            }
+            if (GetCellTileData((int)Layer.Ground, tileMapPos) != null &&
+                !IsBoundary((int)Layer.Ground, tileMapPos))
+            {
+                SetCell((int)Layer.Cursor, tileMapPos, TileSelectedId, TileSelectedAtlas);
+                SelectedCell = tileMapPos;
+            }
+            else
+            {
+                SelectedCell = null;
             }
         }
     }
