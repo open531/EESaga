df8c4b1380048d158e5cda7ff76d2616d0689352
晨枫 <1570753829@qq.com>
2024-04-09 16:17:36 +0800

    feat: :sparkles: add card Double Beat ,Shield Strike,Bash

--- Scripts/Cards/ICard.cs
+++ Scripts/Cards/ICard.cs
@@ -1,4 +1,4 @@
-﻿namespace EESaga.Scripts.Cards;
+namespace EESaga.Scripts.Cards;
 
 
 public interface ICard
@@ -33,6 +33,19 @@ public enum CardTarget
     All,
 }
 
+public enum CardAttackType
+{
+    General,
+    DoubleBeat,
+    Bash,
+}
+
+public enum CardDefenseType
+{
+    General,
+    Consolidate,
+}
+
 public enum CardItemType
 {
     General,
@@ -43,12 +56,15 @@ public enum CardSpecialType
     General,
     Cure,
     Struggle,
+    AttackByDefense,
 }
 
 public class CardInfo(
     CardType cardType, string cardName, string cardDescription, int cardCost, CardTarget cardTarget, int cardRange = 1,
     int attackDamage = 0, int attackTimes = 0,
     int defenseValue = 0,
+    CardAttackType attackType = CardAttackType.General,
+    CardDefenseType defenseType = CardDefenseType.General,
     CardSpecialType specialType = CardSpecialType.General,
     CardItemType itemType = CardItemType.General
     ) : ICard
@@ -78,14 +94,20 @@ public class CardInfo(
 
     #region Item
     public CardItemType ItemType { get; set; } = itemType;
+    public CardAttackType AttackType { get; set; } = attackType;
+    public CardDefenseType DefenseType { get; set; } = defenseType;
     #endregion
 }
 
 public static class CardData
 {
     public static CardInfo CAStrike = new(CardType.Attack, "C_A_STRIKE", "C_A_STRIKE_DESC", 1, CardTarget.Enemy, 1, attackDamage: 6, attackTimes: 1);
+    public static CardInfo CABash = new(CardType.Attack, "C_A_BASH", "C_A_BASH_DESC", 1, CardTarget.Enemy, 1, attackDamage: 6, attackTimes: 1, attackType: CardAttackType.Bash);
+    public static CardInfo CADoubleBeat = new(CardType.Attack, "C_A_DOUBLE_BEAT", "C_A_DOUBLE_BEAT_DESC", 1, CardTarget.Enemy, 1, attackDamage: 4, attackTimes: 2, attackType: CardAttackType.DoubleBeat);
     public static CardInfo CDDefend = new(CardType.Defense, "C_D_DEFEND", "C_D_DEFEND_DESC", 1, CardTarget.Self, defenseValue: 3);
+    public static CardInfo CDConsolidate = new(CardType.Defense, "C_D_CONSOLIDATE", "C_D_CONSOLIDATE_DESC", 1, CardTarget.Self, defenseValue: 0, defenseType: CardDefenseType.Consolidate);
     public static CardInfo CSStruggle = new(CardType.Special, "C_S_STRUGGLE", "C_S_STRUGGLE_DESC", 1, CardTarget.Self, specialType: CardSpecialType.Struggle);
+    public static CardInfo CSShieldStrike = new(CardType.Special, "C_S_SHIELD_STRIKE", "C_S_SHIELD_STRIKE_DESC", 1, CardTarget.Enemy, specialType: CardSpecialType.AttackByDefense);
     public static CardInfo CIECS = new(CardType.Item, "C_I_ECS", "C_I_ECS_DESC", 1, CardTarget.Self, itemType: CardItemType.Ecs);
     public static CardInfo CSCure = new(CardType.Special, "C_S_CURE", "C_S_CURE_DESC", 1, CardTarget.Self, specialType: CardSpecialType.Cure);
 }
