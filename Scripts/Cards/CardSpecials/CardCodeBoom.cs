d49c21964a8d23a00c9d2077726cdf50e3eb7cad
晨枫 <1570753829@qq.com>
2024-04-12 23:29:29 +0800

    feat: :sparkles: Code Boom

--- Scripts/Cards/CardSpecials/CardCodeBoom.cs
+++ Scripts/Cards/CardSpecials/CardCodeBoom.cs
@@ -1,15 +1,45 @@
+using EESaga.Scripts.Cards.CardSpecials;
+using EESaga.Scripts.Entities;
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public partial class CardCodeBoom : Control
+public partial class CardCodeBoom : CardSpecial
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public static int attackDamage = 8;
+    public static new CardCodeBoom Instance() => GD.Load<PackedScene>("res://Scenes/Cards/CardSpecials/card_code_boom.tscn").Instantiate<CardCodeBoom>();
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override List<string> TakeEffect(List<BattlePiece> battlePieces)
+    {
+        var effectInfo = new List<string>
+        {
+            $"{Tr("T_USE")} {Tr("C_S_CODE_BOOM")}\n"
+        };
+        var damageList = new List<List<int>>();
+        var damageInfo = new string("");
+        foreach (var piece in battlePieces)
+        {
+            damageList.Add(piece.BeAttacked(attackDamage));
+            var deathInfo = piece.CheckDeath();
+            foreach (var damage in damageList)
+            {
+                damageInfo += damage[0] == 0
+                    ? ""
+                    : $"{Tr(piece.PieceName)} {Tr("PIECE_SHIELD")} {Tr("T_LOST")} {damage[0]}\n";
+                damageInfo += damage[1] == 0
+                    ? ""
+                    : $"{Tr(piece.PieceName)} {Tr("PIECE_HEALTH")} {Tr("T_REDUCE")} {damage[1]}\n";
+                damageInfo += damage[1] == 0 && damage[0] == 0
+                    ? $"{Tr("T_NO_EFFECT")}\n"
+                    : $"{Tr(piece.PieceName)} {Tr("PIECE_HEALTH")} : {damage[2]}\n";
+            }
+
+            if (deathInfo)
+            {
+                damageInfo += $"{Tr(piece.PieceName)} {Tr("T_DECEASED")}\n";
+            }
+        }
+        effectInfo.Add(damageInfo);
+        return effectInfo;
+    }
 }
